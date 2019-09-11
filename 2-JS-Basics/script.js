4ab12dc4b51e98645188bab9d5c3413ec6933cdb
Uris <urisnicolas@gmail.com>
2019-09-10 22:30:55 -0400

    Section 11 about Operator Precedence

---
+++ 2-JS-Basics/script.js
@@ -0,0 +1,191 @@
+/********************************
+ *Variables and data types
+ */
+/*
+console.log('hello wrold');
+
+var fistName = 'Jhon';
+console.log(fistName);
+
+var lastName = 'Smith';
+var age =28;
+
+var fullAge = true;//boolean
+console.log(fullAge);
+
+var job;//variable undefined
+console.log(job);
+
+job ='Teacher';
+console.log(job);
+
+//Variable naming rules
+var _3years = 3;
+var johnMark = 'Jhon and MArk';
+*/
+
+/********************************
+ *Variables mutation and type coercion
+ */
+/*
+var fistName = "John";
+var age = 28;
+
+console.log(fistName + " " + age);
+
+var job, isMarried;
+job = "teacher";
+isMarried = false;
+
+console.log(
+  fistName +
+    " is a " +
+    age +
+    " year old " +
+    job +
+    ". is he married? " +
+    isMarried
+);
+
+//Variable mutation
+age = "twenty eight";
+job = "driver";
+
+alert(
+  fistName +
+    " is a " +
+    age +
+    " year old " +
+    job +
+    ". is he married? " +
+    isMarried
+);
+
+var lastName = prompt("What is hes last Name?");
+console.log(fistName + " " + lastName);
+*/
+
+/****************************
+ * Basic operators
+ */
+/*
+var year , yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+//Math operators
+yearMark = now - ageMark;
+yearJohn = now - ageJohn;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+//Logical operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+//Typefo operator
+console.log(typeof johnOlder);//typeof me dice el tipo de operador que es una variable.
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
+
+*/
+
+/************************************/
+//OPERATOR PRECEDENCE
+/************************************/
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+
+
+
+
+
+
+
+/*
+
+var massMark = 78; //kg
+var heightMark= 1.69; //meter
+
+var massJohn = 92; //kg
+var heightJohn = 1.95; //meter
+
+var BMIMark = massMark / (heightMark * heightMark);
+var BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+var markHigherBMI = BMIMark > BMIJohn;
+console.log('Is Mark\'s BMI higher than John\'s' + markHigherBMI);
+
+*/
+
+//*********************
+/****
+ * IF / ELSE STATEMENT
+ /***
+
+var firstName ='John';
+var civilStatus = 'single';
+
+if (civilStatus === 'married'){
+    console.log(firstName + ' is married!' );
+} else
+{
+    console.log(firstName + ' will hopefully married soon:) ');
+}
+
+var isMarried = true;
+if (isMarried === true){
+    console.log(firstName + ' is married!' );
+} else
+{
+    console.log(firstName + ' will hopefully married soon:) ');
+}
+ ***/
+/*************************/
+/*
+var massMark = 78; //kg
+var heightMark= 1.69; //meter
+
+var massJohn = 92; //kg
+var heightJohn = 1.95 //meter
+
+var BMIMark = massMark / (heightMark * heightMark);
+var BMIJohn = massJohn / (heightJohn * heightJohn);
+
+if (BMIMark > BMIJohn) {
+    console.log('Mark\' BMI is higher than John\'s.');
+}else {
+    console.log(' John\' BMI is higher than Mark\'s.');
+}
+
+//var markHigherBMI = BMIMark > BMIJohn;
+//console.log('Is Mark\'s BMI higher than John\'s' + markHigherBMI);
+*/
+
+/***************************************************/
+//  BOOLEAN LOGIC
+ /*************************************************/
+
+/*
+var firstName ='John';
+var age = 16;
+
+if(age <13){
+    console.log(firstName + ' is a boy');
+}else if(){ //between 13 and 20 === age >= 13 AND age < 20
+    console.log(firstName + ' is a teenager');
+}
+else {
+    console.log(firstName + ' is a man');
+}
+*/
