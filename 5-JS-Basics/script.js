ec3ed014885831ecadd91a420da430fe1a688d61
Uris <urisnicolas@gmail.com>
2020-04-26 12:54:45 -0800

    Functions returning functions

--- 5-JS-Basics/script.js
+++ 5-JS-Basics/script.js
@@ -142,12 +142,36 @@ console.log(rates);
 // Lecture: Functions returning functions
 
 
+function interviewQuestion(job){
+
+    if(job === 'designer'){
+        return function (name) {
+            console.log(name + ', can you please explain ' +
+                'what UX design is?');
+        }
+    }else if(job === 'teacher') {
+        return function (name) {
+            console.log('what subject do you teach, ' +
+                name + '?');
+        }
+    } else {
+        return function (name) {
+            console.log('Hello ' + name + ',what do you do?');
+
+        }
+    }
+}
+
+var teacherQuestion =
+    interviewQuestion('teacher');
+var designerQuestion =
+    interviewQuestion('designer');
+
+teacherQuestion('John');
+designerQuestion('Maria');
 
 
-
-
-
-
+interviewQuestion('teacher')('Mark');
 
 
 
